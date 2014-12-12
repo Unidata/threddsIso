61e99c9b5b5defd049a3683287d728e5ebfcfa5d
Ethan Davis <edavis@unidata.ucar.edu>
2014-12-12 15:52:27 -0700

    Update for use in TDS 4.5.4:  - Update dependency versions  - Add README.md

--- src/test/java/thredds/client/HttpRequestService.java
+++ src/test/java/thredds/client/HttpRequestService.java
@@ -1,7 +1,7 @@
 package thredds.client;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.methods.GetMethod;
+//import org.apache.commons.httpclient.HttpClient;
+//import org.apache.commons.httpclient.methods.GetMethod;
 
 public class HttpRequestService {
 
@@ -9,13 +9,13 @@ public class HttpRequestService {
         String result = null;
     
         try {
-            HttpClient client = new HttpClient();
-            GetMethod get = new GetMethod(url);
-            get.setQueryString(queryString);
-            client.executeMethod(get);
-        
-            result = get.getResponseBodyAsString();
-            get.releaseConnection();
+//            HttpClient client = new HttpClient();
+//            GetMethod get = new GetMethod(url);
+//            get.setQueryString(queryString);
+//            client.executeMethod(get);
+//
+//            result = get.getResponseBodyAsString();
+//            get.releaseConnection();
         } 
         catch (Exception e) {
             throw new Exception("Get failed", e);
