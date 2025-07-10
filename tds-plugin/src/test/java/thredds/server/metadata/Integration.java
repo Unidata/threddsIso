4860ccc752fffe76da6475fe94ebbb9c8e3391e7
Sean Arms <67096+lesserwhirls@users.noreply.github.com>
2025-07-10 11:20:31 -0600

    Upgrade to netCDF-Java 5.9.0-SNAPSHOT
    Also, now that TDS 5.7-SNAPSHOT bundles in code from tds-plugin, we
    needed to make some some changes to the testcontainer environment to
    ensure it uses the nciso-common and tds-plugin classes from this repo
    and not those bundled with the TDS. Additionally, the integration test
    requiring the testcontainer infrastructure is now run as part of the
    verify lifecycle and excluded from the test lifecycle.

---
+++ tds-plugin/src/test/java/thredds/server/metadata/Integration.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2025 University Corporation for Atmospheric Research/Unidata
+ * See LICENSE for license information.
+ */
+
+package thredds.server.metadata;
+
+public interface Integration { }
